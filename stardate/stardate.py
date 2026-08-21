c1260d4235c7c39276a090ed2798c970da2b324c
sandyrob <FelagundFearless@protonmail.com>
2026-08-21 17:20:57 -0400

    - Updated README.md - Added decToHex(self, dec) and hexToDec(self, hex) to stardate object - Completed functionality of gregToJulianPart(when) and gregToJulianFull(when) - Added functions julToGreg(when: dt.datetime) and julToGregAbr(when:dt.date) - Added functionality to stardate.stardate.setDate(when)

--- stardate/stardate.py
+++ stardate/stardate.py
@@ -1,4 +1,5 @@
 import datetime as dt
+import math
 
 # TODO:
 # [] Figure out library structure
@@ -8,11 +9,14 @@ import datetime as dt
 # [] Nail down zero day
 # [] implement zero day and formulae
 # [] create format for stardates
+# [] Numeric base conversion functions
+#   [*] Decimal to hex
+#   [] hex to dec
 # [] function to convert datetime.datetime to int elements
 #   - use datetime.timedelta?
 #   - https://docs.python.org/3/library/datetime.html
-# [] function to convert gregorian (with time) to Julian
-# [] function to convert greg to jules (sans clock)
+# [*] function to convert gregorian (with time) to Julian
+# [*] function to convert greg to jules (sans clock)
 # [] implement into calculator functions
 #   [] enter date on greg calendar, get stardate out
 #   [] enter stardate, test validity, and get greg date out
@@ -27,18 +31,73 @@ secInMin = 60
 # zeroDayJules = datetime()
 
 
+# Functions
+
+# purpose: convert gregorian date (including time of day) to Julian Calendar
+def gregToJulianFull(when: dt.datetime):
+    # Basic conversion for dates between 1901 and 2099 is Gregorian date minus 13 days
+    # the gap in days grows by 3 every 400 years (0.0075 days per year)
+    j = when - dt.timedelta(days=13.0075)
+    return j
+
+
+# purpose: convert gregorian date (NOT including time of day) to Julian Calendar
+def gregToJulianPart(when: dt.date):
+    # JC = GC - 13
+    j = when - dt.timedelta(days=13.0075)
+    return j
+
+def julToGreg(when:dt.datetime):
+    g = when + dt.timedelta(days=13.0075)
+    return g
+
+def julToGregAbr(when:dt.date):
+    return when + dt.timedelta(days=13.0075)
+
 class stardate:
-    datum: float
+    datum = []*6
 
     def __init__(self):
         # initialize the object
-        self.datum = 0.0
+        self.datum[3] = '.'
 
     def getDate(self):
         return self.datum
 
-    def setDate(self, when):
-        self.datum = when
+    def setDate(self, when: list):
+        for i in when:
+            if i == '.':
+                continue
+            else:
+                    self.datum[i] = when[i]
+
+    def decToHex(self, dec: int):
+        hex = ""
+        buff = []
+        numLett = ['A', 'B', 'C', 'D', 'E', 'F']
+
+        # do the math
+        while dec > 0:
+            # if the remainder > 9, it's a letter not 10-16
+            if (dec%16) >= 10 and not((dec%16) > 15):
+                buff.append(numLett[(dec%16)-10])
+            else:
+                # remainder is not >9
+                buff.append(dec%16)
+
+            # amend dec to reflect dec/16 without the remainder as floats
+            dec = math.floor(dec/16)
+
+        # assemble the remainders into the hex number
+        for i in range(5, 0, -1):
+            hex += buff[i]
+
+        return hex
+
+    def hexToDec(self, hex):
+        dec = 0
+
+        return dec
 
     def calcDate(self, Jules: dt.datetime):
         # take the parts of the date and calculate the stardate (INCLUDING HOURS/MINUTES/SECONDS)
@@ -63,20 +122,4 @@ class stardate:
 
         when += minute/10000
 
-
-# Functions
-
-# purpose: convert gregorian date (including time of day) to Julian Calendar
-def gregToJulianFull(self, when: dt.datetime):
-    # Basic conversion for dates between 1901 and 2099 is Gregorian date minus 13 days
-    # the gap in days grows by 3 every 400 years (0.0075 days per year)
-    j = 1
-    return j
-
-
-# purpose: convert gregorian date (NOT including time of day) to Julian Calendar
-def gregToJulianPart(self, when: dt.date):
-    # JC = GC - 13
-    j = 1
-    return j
 
