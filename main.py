cfe920a4b933d8cab1ab97761feb2c416525876b
sandyrob <FelagundFearless@protonmail.com>
2026-08-27 14:04:38 -0400

    - Updated TODO - Added import statement for tkinter - Amended previous import statements to provide aliases for libraries

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
-import datetime
-import stardate
+import datetime as dt
+#import stardate as sd
+import tkinter as tk
 
 # TODO:
 # [*] stardate library/module setup
 # [*] Class for stardate objects
+# [] complete stardate.py and build library
 # [] implement function to convert rightNow into present stardate
 #   - threaded and continuously running
 # [] GUI
 
 
-rightNow = datetime.datetime.now()
+rightNow = dt.datetime.now()
 
 # Purpose: convert rightNow to Julian calendar and then use timedelta to calculate stardate
 def rnStardate():
